395d398a4f58b55cdd5c1e70c3b27f9c326992dd
Tyler Sengia <tylersengia@gmail.com>
2020-11-18 01:09:32 -0500

    Changed version and updated URL

--- setup.py
+++ setup.py
@@ -2,15 +2,14 @@ from setuptools import setup
 
 setup(
     name='ujlaser',
-    version='0.9',
+    version='1.0',
     description='Library to control a Quantum Composers MicroJewel Laser.',
-    url='https://github.com/nschaffin/OASIS-Laser',
+    url='https://github.com/Student-Space-Programs-Laboratory/py-ujlaser',
     author='Miles Green, Noah Chaffin, Tyler Sengia',
     author_email='tylersengia@gmail.com',
     license='The Unlicense',
     packages=['ujlaser'],
     install_requires=['pyserial>=3.0'],
     classifiers=['Programming Language :: Python :: 3',
-                 'Programming Language :: Python :: 3.4',
                  'Programming Language :: Python :: 3.5'],
 )
